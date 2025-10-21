db9f9a99f2076254ca8a375747bd603384d69a2e
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-10-21 16:25:26 -0600

    21-10-25 feat: added Profesor insert query and Implementation

--- src/java/dominio/ProfesorImp.java
+++ src/java/dominio/ProfesorImp.java
@@ -4,6 +4,7 @@
  */
 package dominio;
 
+import dto.Respuesta;
 import java.util.List;
 import modelo.mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
@@ -30,4 +31,32 @@ public class ProfesorImp {
         return profesores;
     }
 
+    public static Respuesta registrar(Profesor profesor) {
+        Respuesta respuesta = new Respuesta();
+
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if (conexionBD != null) {
+            try {
+                int filasAfectadas = conexionBD.insert("profesor.registrar", profesor);
+                conexionBD.commit();
+
+                if (filasAfectadas > 0) {
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Profesor " + profesor.getNombre() + " " + profesor.getApellidoPaterno() + " registrado exitosamente.");
+                } else {
+                    respuesta.setError(true);
+                    respuesta.setMensaje("Error al registrar al profesor.");
+                }
+                conexionBD.close();
+            } catch (Exception e) {
+                respuesta.setError(true);
+                respuesta.setMensaje(e.getMessage());
+            }
+        } else {
+            respuesta.setError(true);
+            respuesta.setMensaje("No hubo conexión con la Base de Datos");
+        }
+
+        return respuesta;
+    }
 }
--- src/java/ws/ProfesorWS.java
+++ src/java/ws/ProfesorWS.java
@@ -4,9 +4,15 @@
  */
 package ws;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import dominio.ProfesorImp;
+import dto.Respuesta;
 import java.util.List;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -24,5 +30,19 @@ public class ProfesorWS {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Profesor> obtenerTodos() {
         return ProfesorImp.obtenerTodos();
+    }
+
+    @Path("registrar")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Respuesta registrar(String json) {
+        Gson gson = new Gson();
+        try {
+            Profesor profesor = gson.fromJson(json, Profesor.class);
+            return ProfesorImp.registrar(profesor);
+        } catch (JsonSyntaxException e) {
+            throw new BadRequestException(e.getMessage());
+        }
     }
 }
