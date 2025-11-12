c1f317b0c11075167ff5bf380f44f9320fe8ed43
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-11-11 22:09:26 -0600

    11-11-25 feat: added Alumno implementation, mapper and WS. [obtener-Todos, obtener-Todos-Carrera]

--- src/java/ws/AlumnoWS.java
+++ src/java/ws/AlumnoWS.java
@@ -4,7 +4,15 @@
  */
 package ws;
 
+import dominio.AlumnoImp;
+import java.util.List;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import pojo.Alumno;
 
 /**
  *
@@ -12,5 +20,21 @@ import javax.ws.rs.Path;
  */
 @Path("alumno")
 public class AlumnoWS {
-    
+
+    @Path("obtener-todos")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Alumno> obtenerAlumnos() {
+        return AlumnoImp.obtenerAlumnos();
+    }
+
+    @Path("obtener-todos-carrera/{idCarrera}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Alumno> obtenerPorCarrera(@PathParam("idCarrera") Integer idCarrera) {
+        if (idCarrera != null && idCarrera > 0) {
+            return AlumnoImp.obtenerAlumnosPorCarrera(idCarrera);
+        }
+        throw new BadRequestException();
+    }
 }
