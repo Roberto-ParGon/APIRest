2d8bee99c77f7837a17f43b8bde561ac443e0311
Roberto-ParGon <zs19016407@estudiantes.uv.mx>
2025-11-11 22:37:55 -0600

    11-11-25 feat: added Catalogo implementation, mapper and WS. [Obtener-Facultades, Obtener-Carreras-Facultad]

--- src/java/dominio/CatalogoImp.java
+++ src/java/dominio/CatalogoImp.java
@@ -7,6 +7,8 @@ package dominio;
 import java.util.List;
 import modelo.mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import pojo.Carrera;
+import pojo.Facultad;
 import pojo.Rol;
 
 /**
@@ -32,4 +34,36 @@ public class CatalogoImp {
 
         return roles;
     }
+
+    public static List<Facultad> obtenerFacultades() {
+        List<Facultad> facultades = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if (conexionBD != null) {
+            try {
+                facultades = conexionBD.selectList("catalogo.obtener-facultades");
+                conexionBD.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return facultades;
+    }
+
+    public static List<Carrera> obtenerCarrerasFacultad(int idFacultad) {
+        List<Carrera> carreras = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if (conexionBD != null) {
+            try {
+                carreras = conexionBD.selectList("catalogo.obtener-carreras-facultad", idFacultad);
+                conexionBD.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return carreras;
+    }
+
 }
--- src/java/ws/CatalogoWS.java
+++ src/java/ws/CatalogoWS.java
@@ -6,10 +6,14 @@ package ws;
 
 import dominio.CatalogoImp;
 import java.util.List;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import pojo.Carrera;
+import pojo.Facultad;
 import pojo.Rol;
 
 /**
@@ -24,5 +28,22 @@ public class CatalogoWS {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Rol> obtenerRoles() {
         return CatalogoImp.obtenerRoles();
+    }
+
+    @Path("obtener-facultades")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Facultad> obtenerFacultades() {
+        return CatalogoImp.obtenerFacultades();
+    }
+
+    @Path("obtener-carreras-facultad/{idFacultad}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Carrera> obtenerCarrerasFacultad(@PathParam("idFacultad") Integer idFacultad) {
+        if (idFacultad != null && idFacultad > 0) {
+            return CatalogoImp.obtenerCarrerasFacultad(idFacultad);
+        }
+        throw new BadRequestException();
     }
 }
